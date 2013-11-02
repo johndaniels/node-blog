cc7a708606479cb0706c54a89d710f12d4531409
John Daniels <johnsdaniels@gmail.com>
2013-11-02 10:22:43 -0400

    Added an api that actually reads from a real database.

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 (function () {
     "use strict";
     var express = require('express');
+    var sqlite3 = require('sqlite3');
+    var db = new sqlite3.Database('blog.db');
     var app = express();
     app.use("/static", express.static('static'));
 
-    app.get('/api/v0/posts/1', function(req, res) {
-        res.json({
-            title: 'My first post',
-            body: 'Hello, world'
+    app.get('/api/v0/posts/:id', function(req, res) {
+        var id = parseInt(req.params.id, 10);
+        db.get('SELECT title, body FROM Posts WHERE id = ?', id, function (err, post) {
+            res.json(post);
         });
     });
 
