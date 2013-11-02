5c7e969004c08e4f34c7e694f092f4622ca26b5c
John Daniels <johnsdaniels@gmail.com>
2013-11-02 12:14:49 -0400

    Add a backbone endpoint that lists blog posts.

--- static/main.js
+++ static/main.js
@@ -3,6 +3,9 @@
     
     var Post = Backbone.Model.extend({
         urlRoot: '/api/v0/posts',
+        fragment: function () {
+            return '/posts/' + this.id;
+        },
         title: function () {
             return this.get('title');
         },
@@ -11,14 +14,24 @@
         }
     });
 
+    var PostCollection = Backbone.Collection.extend({
+        url: '/api/v0/posts',
+        model: Post
+    });
+
     var postTemplate = Mustache.compile(
         '<h1>{{title}}</h1><div id="post-body">{{body}}</div>'
+    );
+
+    var postsTemplate = Mustache.compile(
+        '{{#posts}}<div><a href="#{{fragment}}">{{title}}</a></div>{{/posts}}'
     );
 
     var Router = Backbone.Router.extend({
         routes: {
             'about': 'about',
-            'posts/:id': 'post'
+            'posts/:id': 'post',
+            'posts': 'posts'
         },
 
         about: function() {
@@ -31,6 +44,14 @@
             });
             post.fetch().done(function () {
                 $('#main').html(postTemplate(post));
+            });
+        },
+        posts: function () {
+            var posts = new PostCollection();
+            posts.fetch().done(function () {
+                $('#main').html(postsTemplate({
+                    posts: posts.models
+                }));
             });
         }
     });
