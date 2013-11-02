3a7c58920a26d66dea9951347c53006edeefdf11
John Daniels <johnsdaniels@gmail.com>
2013-11-02 11:38:13 -0400

    Add post listing endpoint.

--- app.js
+++ app.js
@@ -6,12 +6,21 @@
     var app = express();
     app.use("/static", express.static('static'));
 
+    app.get('/api/v0/posts', function(req, res) {
+        var id = parseInt(req.params.id, 10);
+        db.all('SELECT id, title, body FROM Posts', function (err, posts) {
+            console.log(posts);
+            res.json(posts);
+        });
+    });
+
     app.get('/api/v0/posts/:id', function(req, res) {
         var id = parseInt(req.params.id, 10);
         db.get('SELECT title, body FROM Posts WHERE id = ?', id, function (err, post) {
             res.json(post);
         });
     });
+
 
     app.get('/', function (req, res) {
         res.sendfile("main.html");
