1fd540ced4b371c1258cf778d04af119f650c6ab
John Daniels <johnsdaniels@gmail.com>
2013-11-02 08:54:08 -0400

    Set up a rock stupid Json api that only returns one post.

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 (function () {
     var express = require('express');
     var app = express();
-    app.use(express.static('static'));
+    app.use("/static", express.static('static'));
+
+    app.get('/api/v0/posts/1', function(req, res) {
+        res.json({
+            title: 'My first post',
+            body: 'Hello, world'
+        });
+    });
 
     app.listen('8080');
 })();
